d155e0c57e12d852d10f6f35f43ff12a11f0805b
Admin <admin@Admins-Mac.local>
2019-07-08 22:10:04 +0300

    first message

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, world!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome Button", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)        
+    }
     
 }
 
